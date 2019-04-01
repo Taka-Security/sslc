c4b6b4337c02fe4b03e1a1bee966788ef440a12f
Alexander Remie <user@rmi7.me>
2019-04-01 17:56:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,113 @@
+# sslc
+
+**s**olidity **s**truct **l**ayout **c**hecker
+
+![License](https://img.shields.io/github/license/rmi7/sslc.svg?style=flat-square)
+[![Version](https://img.shields.io/npm/v/sslc.svg?style=flat-square&label=version)](https://www.npmjs.com/package/sslc)
+![Download](https://img.shields.io/npm/dt/sslc.svg)
+
+Given some Solidity smart contract(s), extract info about all structs. Ending with a list of structs which could be stored more efficiently by reordering the struct member variables.
+
+## Dependencies
+
+Makes use of [solidity-parser-antlr](https://github.com/federicobond/solidity-parser-antlr) to parse Solidity files.
+
+## Usage
+
+```
+usage: sslc [-h] [-v] -f path [path ...] [--only-last]
+
+Solidity struct layout checker
+
+Optional arguments:
+  -h, --help          Show this help message and exit.
+  -v, --version       Show program's version number and exit.
+  -f path [path ...]  input solidity file(s), supports glob
+  --only-last         use only last contract in input file
+```
+
+#### without install
+
+```
+npx sslc ~/my-solidity-project/contracts/*.sol
+```
+
+#### with install
+
+```
+npm i -g sslc
+sslc ~/my-solidity-project/contracts/*.sol
+```
+
+## Output
+
+The output will be a single Solidity file printed to stdout. Example output:
+
+```Solidity
+// solidity file: /some/path/SomeContract.sol
+struct Mystruct {
+
+  uint8 myFirstVar; // bytes: 1
+  //---------- end of slot 1 | bytes in: 1 | bytes left: 31
+  
+  uint256 mySecondVar; // bytes: 32
+  //---------- end of slot 2 | bytes in: 32 | bytes left: 0
+  
+  uint16 myThirdVar; // bytes: 2
+  //---------- end of slot 3 | bytes in: 2 | bytes left: 30
+  
+} // current slot count = 3 | optimized slot count = 2
+
+// STRUCTS THAT CAN BE OPTIMIZED
+// =============================
+// file: /some/path/SomeContract.sol
+// current num storage slots: 3
+// possible num storage slots: 2
+// -----------------------------
+```
+
+## TODO
+
+### Optimized memory layout algorithm
+
+The current algorithm to calculate the most efficient memory layout simply sorts all member variables based on byte size ascending, and checks how much storage slots this layout occupies. There is room for improvement here as this sometimes leads to not the most efficient layout. As shown in the following sample.
+
+#### Actual file
+
+```Solidity
+struct MyStruct {
+  uint32 var1; // bytes: 4
+  uint88 var2; // bytes: 11
+  uint88 var3; // bytes: 11
+  //---------- end of slot 1 | bytes in: 26 | bytes left: 6
+
+  uint88 var4; // bytes: 11
+  uint88 var5; // bytes: 11
+  uint64 var6; // bytes: 8
+  //---------- end of slot 2 | bytes in: 30 | bytes left: 2
+}
+```
+
+#### Assumed-to-be-most-efficient file by current algorithm
+
+```Solidity
+struct MyStruct {
+  uint32 var1; // bytes: 4
+  uint64 var6; // bytes: 8
+  uint88 var2; // bytes: 11
+  //---------- end of slot 1 | bytes in: 23 | bytes left: 9
+  
+  uint88 var3; // bytes: 11
+  uint88 var4; // bytes: 11
+  //---------- end of slot 2 | bytes in: 22 | bytes left: 10
+
+  uint88 var5; // bytes: 11
+  //---------- end of slot 3 | bytes in: 11 | bytes left: 21
+}
+```
+
+The current algorithm's most efficient layout takes one more storage slot than the actual struct layout.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,227 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const solidityParser = require('solidity-parser-antlr');
+const { ArgumentParser } = require('argparse');
+
+const uintRegex = /uint(\d+)/m;
+const intRegex = /int(\d+)/m;
+const bytesRegex = /bytes(\d+)/m;
+
+const calcStructStorageSlotCount = (structMembers) => {
+  let slotCount = 1;
+  let storageSlotByteCount = 0;
+  structMembers.forEach((targetMember) => {
+    if (targetMember.varByteSize + storageSlotByteCount > 32) {
+      storageSlotByteCount = targetMember.varByteSize;        
+      slotCount += 1;
+    } else {
+      storageSlotByteCount += targetMember.varByteSize;
+    }
+  });
+  return slotCount;
+};
+
+const argParser = new ArgumentParser({
+  version: '0.0.1',
+  addHelp: true,
+  description: 'Solidity struct layout checker',
+});
+
+argParser.addArgument(
+  [ '-f' ],
+  {
+    nargs: '+',
+    required: true,
+    metavar: 'path',
+    help: 'input solidity file(s), supports glob',
+    dest: 'input_files',
+  },
+);
+
+argParser.addArgument(
+  '--only-last',
+  {
+    type: Boolean,
+    defaultValue: false,
+    help: 'use only last contract in input file',
+    dest: 'only_last_contract',
+    action: 'storeTrue',
+  },
+);
+
+const args = argParser.parseArgs();
+
+const summary_can_be_more_efficient = {};
+
+try {
+  args.input_files.forEach((input_file) => {    
+    const results = {};
+
+    const input = fs.readFileSync(input_file, 'utf8');
+    const ast = solidityParser.parse(input, { loc: true });
+
+    const structs = args.only_last_contract
+      ? ast.children[ast.children.length - 1].subNodes.filter(n => n.type === 'StructDefinition')
+      : ast.children.filter(child => child.type === 'ContractDefinition')
+                    .map(child => child.subNodes.filter(n => n.type === 'StructDefinition'))
+                    .flat();
+    
+    structs.forEach((targetStruct) => {
+      const structResult = [];
+      targetStruct.members.forEach((targetMember) => {
+        switch (targetMember.typeName.type) {
+          case 'Mapping':
+            structResult.push({
+              varName: targetMember.name,
+              varKind: 'mapping',
+              varByteSize: 32,
+              varLine: targetMember.loc.start.line - 1,
+            });
+            return;
+          case 'ArrayTypeName':
+            structResult.push({
+              varName: targetMember.name,
+              varKind: 'array',
+              varByteSize: 32,
+              varLine: targetMember.loc.start.line - 1,
+            });
+            return;
+          case 'ElementaryTypeName':
+            let targetMemberType = targetMember.typeName.name;
+            
+            if (targetMemberType.startsWith('int')) {
+              if (targetMemberType === 'int') targetMemberType = 'int256';
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary',
+                varByteSize: parseInt(targetMemberType.match(intRegex)[1], 10) / 8,
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;
+            }
+            
+            if (targetMemberType.startsWith('uint')) {
+              if (targetMemberType === 'uint') targetMemberType = 'uint256';
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary',
+                varByteSize: parseInt(targetMemberType.match(uintRegex)[1], 10) / 8,
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;
+            }
+            
+            if (targetMemberType === 'bytes') {
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary', // it's really a dynamic array
+                varByteSize: 32,
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;  
+            }
+            
+            if (targetMemberType.startsWith('bytes')) {
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary',
+                varByteSize: parseInt(targetMemberType.match(bytesRegex)[1], 10),
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;
+            }
+            
+            if (targetMemberType === 'string') {
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary', // it's really a dynamic array
+                varByteSize: 32,
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;  
+            }
+            
+            if (targetMemberType === 'bool') {
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary', // it's really a dynamic array
+                varByteSize: 1,
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;  
+            }
+            
+            if (targetMemberType === 'address') {
+              structResult.push({
+                varName: targetMember.name,
+                varType: targetMemberType,
+                varKind: 'elementary',
+                varByteSize: 20,
+                varLine: targetMember.loc.start.line - 1,
+              });
+              return;  
+            }
+        }
+      });
+      results[targetStruct.name] = structResult;
+    });
+    
+    Object.keys(results).forEach((structName) => {
+      console.log(`\n// solidity file: ${input_file}`);
+      console.log(`struct ${structName} {\n`);
+      let slotCount = 1;
+      let storageSlotByteCount = 0;
+      results[structName].forEach((targetMember) => {
+        if (targetMember.varByteSize + storageSlotByteCount > 32) {
+          console.log('  //' + '-'.repeat(10) + ` end of slot ${slotCount} | bytes in: ${storageSlotByteCount} | bytes left: ${32 - storageSlotByteCount}\n`);
+          storageSlotByteCount = targetMember.varByteSize;        
+          slotCount += 1;
+        } else {
+          storageSlotByteCount += targetMember.varByteSize;
+        }
+        console.log(`  ${input.split('\n')[targetMember.varLine].replace(/\/\/.+/, '').trim()} // bytes: ${targetMember.varByteSize}`);
+      });
+      
+      const optimizedSlotCount = calcStructStorageSlotCount(results[structName].sort((a, b) => a.varByteSize - b.varByteSize));
+      summary_can_be_more_efficient[input_file] = { [structName]: { current: slotCount, optimized: optimizedSlotCount } };
+      console.log('  //' + '-'.repeat(10) + ` end of slot ${slotCount} | bytes in: ${storageSlotByteCount} | bytes left: ${32 - storageSlotByteCount}`);
+      console.log(`\n} // current slot count = ${slotCount} | optimized slot count = ${optimizedSlotCount}\n`);
+    });
+  });
+  
+  console.log('// STRUCTS THAT CAN BE OPTIMIZED');
+  console.log('// =============================');
+  let foundStructThatCanBeOptimized = false;
+  Object.keys(summary_can_be_more_efficient).forEach((inputFilePath) => {
+    Object.keys(summary_can_be_more_efficient[inputFilePath]).forEach((structName) => {
+      if (summary_can_be_more_efficient[inputFilePath][structName].current > summary_can_be_more_efficient[inputFilePath][structName].optimized) {
+        if (!foundStructThatCanBeOptimized) {
+          foundStructThatCanBeOptimized = true;
+        }
+        console.log(`// file: ${inputFilePath}`);
+        console.log(`// current num storage slots: ${summary_can_be_more_efficient[inputFilePath][structName].current}`);
+        console.log(`// possible num storage slots: ${summary_can_be_more_efficient[inputFilePath][structName].optimized}`);
+        console.log('// -----------------------------');
+      }
+    });
+  });
+  
+  if (!foundStructThatCanBeOptimized) {
+    console.log('// All structs seem to be efficiently laid out in memory');
+  }
+  
+} catch (e) {
+  if (e instanceof solidityParser.ParserError) {
+    console.log('solidity parser error', e.errors)
+  } else {
+    throw e;
+  }
+}
