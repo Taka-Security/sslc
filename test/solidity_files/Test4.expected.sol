60c52cf850e08b754b7e20403d304bc3d41ce8c4
Alexander Remie <user@rmi7.me>
2019-04-02 15:33:40 +0200

    fix test output order

--- test/solidity_files/Test4.expected.sol
+++ test/solidity_files/Test4.expected.sol
@@ -1,3 +1,16 @@
+struct StructA { // file: Test4.sol | contract: A
+
+  uint8 var1; // bytes: 1
+  //---------- end of slot 1 | bytes taken: 1 | bytes free: 31
+
+  uint var2; // bytes: 32
+  //---------- end of slot 2 | bytes taken: 32 | bytes free: 0
+
+  address var3; // bytes: 20
+  //---------- end of slot 3 | bytes taken: 20 | bytes free: 12
+
+} // current slot count = 3 | optimized slot count = 2
+
 struct StructB { // file: Test4.sol | contract: A
 
   bool var1; // bytes: 1
@@ -23,19 +36,6 @@ struct StructB { // file: Test4.sol | contract: A
 
 } // current slot count = 7 | optimized slot count = 4
 
-struct StructA { // file: Test4.sol | contract: A
-
-  uint8 var1; // bytes: 1
-  //---------- end of slot 1 | bytes taken: 1 | bytes free: 31
-
-  uint var2; // bytes: 32
-  //---------- end of slot 2 | bytes taken: 32 | bytes free: 0
-
-  address var3; // bytes: 20
-  //---------- end of slot 3 | bytes taken: 20 | bytes free: 12
-
-} // current slot count = 3 | optimized slot count = 2
-
 struct StructA { // file: Test4.sol | contract: B
 
   uint8 var1; // bytes: 1
@@ -54,15 +54,15 @@ struct StructA { // file: Test4.sol | contract: B
 // =============================
 // file: Test4.sol
 // contract: A
-// struct: StructB
-// current num storage slots: 7
-// possible num storage slots: 4
-// -----------------------------
-// file: Test4.sol
-// contract: A
 // struct: StructA
 // current num storage slots: 3
 // possible num storage slots: 2
+// -----------------------------
+// file: Test4.sol
+// contract: A
+// struct: StructB
+// current num storage slots: 7
+// possible num storage slots: 4
 // -----------------------------
 // file: Test4.sol
 // contract: B
